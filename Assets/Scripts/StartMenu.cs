828ae67031b22376503b72cd1a8e3edffc26bfaf
l1ttlelol <louis@cowlibob.co.uk>
2023-05-19 11:37:31 +0100

    Created a Start Menu
    Created a start menu with controls and start and quit buttons

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public Button FirstButtonSelect;
+
+    void Start()
+    {
+        FirstButtonSelect.Select();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
